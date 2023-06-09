f7bd1c7879757d6ba0340f3404766c2ce8017606
Joseph Odunsi <odunsiolakunbi@gmail.com>
2023-06-09 18:54:34 +0100

    update main layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,26 @@
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { authOptions } from "~lib/auth";
+
+const navItems = [
+  {
+    name: "Library",
+    href: "/",
+  },
+  {
+    name: "Archive",
+    href: "/archive",
+  },
+  {
+    name: "Trash",
+    href: "/trash",
+  },
+  {
+    name: "Account",
+    href: "/account",
+  },
+];
 
 export default async function Layout({
   children,
@@ -11,5 +31,31 @@ export default async function Layout({
 
   if (!session) redirect("/signin");
 
-  return <section>{children}</section>;
+  return (
+    <section className="relative grid min-h-screen grid-cols-12 gap-6 bg-[#130F40] px-6 py-8 text-[#FEF8FD]">
+      <div className="sticky col-span-2 h-fit items-start space-y-5 rounded-3xl bg-[#1E1633] px-3 py-4">
+        <div>
+          <h1 className="font-mono text-2xl font-bold text-[#FFEC78] lg:text-3xl">
+            Re-Me
+          </h1>
+          <p className="text-sm text-[#93A3B6]">a bookmarking reminder app</p>
+        </div>
+
+        <ul className="text-[#FEF8FD]">
+          {navItems.map((item) => (
+            <li
+              key={item.name}
+              className="w-fit cursor-pointer text-lg hover:underline"
+            >
+              <Link href={item.href}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="col-span-10 rounded-3xl bg-[#1E1633] py-10">
+        {children}
+      </div>
+    </section>
+  );
 }
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { getServerSession } from "next-auth";
 import LibraryItem from "~components/libraryItem";
 import { authOptions } from "~lib/auth";
@@ -29,42 +28,18 @@ export default async function IndexPage() {
   const data = await getData();
 
   return (
-    <section className="relative grid min-h-screen grid-cols-12 gap-6 bg-[#130F40] px-6 py-8 text-[#FEF8FD]">
-      <div className="sticky col-span-2 h-fit items-start space-y-5 rounded-3xl bg-[#1E1633] px-3 py-4">
-        <div>
-          <h1 className="font-mono text-2xl font-bold text-[#FFEC78] lg:text-3xl">
-            Re-Me
-          </h1>
-          <p className="text-sm text-[#93A3B6]">a bookmarking reminder app</p>
-        </div>
-
-        <ul className="text-[#FEF8FD]">
-          <li className="cursor-pointer">
-            <Link href="/">Library</Link>
-          </li>
-          <li className="cursor-pointer">
-            <Link href="/account">Account</Link>
-          </li>
-        </ul>
+    <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+      <div className="mb-6 flex items-end justify-between gap-4">
+        <h2 className="text-2xl font-bold uppercase text-[#FFEC78] lg:text-3xl">
+          library
+        </h2>
       </div>
 
-      <div className="col-span-10 rounded-3xl bg-[#1E1633] py-10">
-        <div className="">
-          <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-            <div className="mb-6 flex items-end justify-between gap-4">
-              <h2 className="text-2xl font-bold uppercase text-[#FFEC78] lg:text-3xl">
-                library
-              </h2>
-            </div>
-
-            <div className="grid gap-x-4 gap-y-8 px-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3">
-              {data.map((item: Item) => (
-                <LibraryItem item={item} key={item.id} />
-              ))}
-            </div>
-          </div>
-        </div>
+      <div className="grid gap-x-4 gap-y-8 px-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3">
+        {data.map((item: Item) => (
+          <LibraryItem item={item} key={item.id} />
+        ))}
       </div>
-    </section>
+    </div>
   );
 }

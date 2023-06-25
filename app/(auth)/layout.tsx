b480c3e556d1acced06c0ede836b546b75326797
Joseph Odunsi <odunsiolakunbi@gmail.com>
2023-06-25 22:02:52 +0100

    change auth bg

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -39,10 +39,12 @@ export default async function Layout({
 
   return (
     <div className="grid min-h-screen grid-cols-12 bg-gradient-to-b from-[#0D0B25] from-20% via-[#261C92] via-100% to-[#93A3B6] to-100%">
-      <div className="col-span-3 flex flex-col items-center justify-center gap-4 bg-[url('/auth-bg.jpg')] bg-cover text-[#FFE169]">
-        <h1 className="text-5xl font-semibold">Welcome to</h1>
+      <div className="col-span-3 flex flex-col items-center justify-center gap-4 bg-[url('/auth-bg.png')] bg-cover text-[#FFE169]">
+        <h1 className="text-5xl font-semibold uppercase">Welcome to</h1>
         <Icons.logo className="h-64 w-52" />
-        <h1 className="text-8xl uppercase">re-me</h1>
+        <div className="self-stretch bg-[#13143B] py-6">
+          <h1 className="text-center font-mono text-8xl">Re-Me</h1>
+        </div>
       </div>
       <div className="col-span-9 grid place-content-center">{children}</div>
     </div>
